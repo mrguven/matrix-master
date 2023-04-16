88c966818d07c0f8ab74e6825e206503387ac0a9
Rauf Guven <mrguven@gmail.com>
2023-04-17 00:41:50 +0200

    update

--- nodeJs/node.js
+++ nodeJs/node.js
@@ -1,5 +1,20 @@
 const http = required ("http");
+var nodemailer = require("nodemailer");
 
 http.createServer (function(req,res) {
     res.write("")
 }).listen(8080);
+
+var transport = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user:'',
+        pass:'yourpassword'
+    }
+});
+
+var mailOptions = {
+    from:'myemail@gmail.com',
+    to:recievers,
+subject:'email ',
+text:'text was easy'}
