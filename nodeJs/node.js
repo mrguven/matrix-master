06d9cd649fb2135bfa08dc7e7c2f529878fd7e8e
Rauf Guven <mrguven@gmail.com>
2023-04-17 10:57:16 +0200

    update

--- nodeJs/node.js
+++ nodeJs/node.js
@@ -2,7 +2,7 @@ const http = required ("http");
 var nodemailer = require("nodemailer");
 
 http.createServer (function(req,res) {
-    res.write("")
+    res.writeHead(200,{'Content-Type':'text/html'})
 }).listen(8080);
 
 var transport = nodemailer.createTransport({
