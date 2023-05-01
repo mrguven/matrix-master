b8c0c86e59850086499f8df2e9b8177c5a6e4ade
Rauf Guven <mrguven@gmail.com>
2023-05-01 11:14:35 +0200

    update

--- week 2/The Calculator/The Calculator.js
+++ week 2/The Calculator/The Calculator.js
@@ -1,30 +1,8 @@
 
 
-
-// let one= document.getElementById("one");
-// let two= document.getElementById("two");
-// let three= document.getElementById("three");
-// let four= document.getElementById("four");
-// let five= document.getElementById("five");
-// let six= document.getElementById("six");
-// let seven= document.getElementById("seven");
-// let eight= document.getElementById("eight");
-// let nine= document.getElementById("nine");
-// let plus= document.getElementById("plus");
-// let min= document.getElementById("min");
-// let ac= document.getElementById("ac");
-// let multiple= document.getElementById("multiple");
-// let divide= document.getElementById("divide");
-// let point= document.getElementById("point");
-// let del= document.getElementById("del");
-// let equal= document.getElementById("=");
-
-
 let result = document.getElementById('result');
 result.innerHTML="";
  
-
-
 let currentNumber="";
 
 let total=0;
@@ -32,20 +10,9 @@ let total=0;
 function myFunction(value) {    
        
       //  console.log(currentNumber);
-       
-
-currentNumber+=value
-
-   
-
+       currentNumber+=value
    result.innerHTML+=value;
-    
- 
-    
-
     return currentNumber;
-
-    
 };
 
 function clearScreen() {
@@ -69,7 +36,7 @@ result.innerHTML=currentNumber;
 
         var firstNumber="";
         var secNumber="";
-        
+
         if(op=='+' && currentNumber) {
             console.log(currentNumber);
             currentNumber=firstNumber;
