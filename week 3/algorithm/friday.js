e368036c5d65181f088a0f94715058558b78bce1
Rauf Guven <mrguven@gmail.com>
2023-05-05 01:53:46 +0200

    update

--- week 3/algorithm/friday.js
+++ week 3/algorithm/friday.js
@@ -1,6 +1,19 @@
 //3rd Highest Number in Array
 
 
+//create a function 
+// create variable, their name ara firNumber,secNumber, thirdNumber
+//use for loop check each element of array
+//use if conditions
+// if one element of highestNumber is bigger then firNumber
+// first, thirdNumber equal secNumber
+//then, secNumber equal to firNumber
+//and firNumber equal to highestNumber[i]
+//the logic is we begin smallest number. and we equalize to groter one.
+// in first if statement we find firNumber(highest number in array)
+//in the second if statement we find mid number
+// in de third if statement we find 3rd highest number
+
 
 let highestNumber= [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
 
@@ -9,24 +22,36 @@ function findNumber (highestNumber) {
         let firNumber=0;
         let secNumber=0;
         let thirdNumber=0;
-        let newArray=[];
+       
 
 
     for(let i=0;i<highestNumber.length;i++) {
+        
 
         if(highestNumber[i]>firNumber) {
+            thirdNumber=secNumber;
+            secNumber=firNumber;
+                firNumber=highestNumber[i];      
+        
+            }
 
-        }
+           else  if(highestNumber[i]>secNumber) {
+                thirdNumber=secNumber;
+                secNumber=highestNumber[i];
+                
 
+            }
+            else if(highestNumber[i]>thirdNumber) {
+                thirdNumber=highestNumber[i];
 
-
+            }
     }
 
-
+return thirdNumber;
         
 }
 
-findNumber(highestNumber);
+console.log(findNumber(highestNumber));
 
 
 
@@ -36,6 +61,9 @@ findNumber(highestNumber);
 
 
 //Friday: Fibonacci Sequence
+
+
+
 
 
 
@@ -56,7 +84,7 @@ for (let i=0;i<fib;i++) {
 
 
 }
-return fibArray;
+return fibArray[fib-1]; // we use min (-1) because we have zero in the array. thus we must ignore it. 
 }
 
 console.log(Fibonacci(43));
