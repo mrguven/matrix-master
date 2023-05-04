2e53de16ad31e9f5d0f3be76a7017dc3b9ac1901
Rauf Guven <mrguven@gmail.com>
2023-05-04 22:38:28 +0200

    update

--- week 3/advanceJs/EcmaScriptExercises.js
+++ week 3/advanceJs/EcmaScriptExercises.js
@@ -83,3 +83,73 @@ const user2 = {
 //We have an object storing salaries of our team:
 
 
+let salaries = {
+    Fred: 100,
+    Ted: 160,
+    Ghaith: 130
+  }
+
+//   let sum= obj => Object.values(obj).reduce((a, b) => a + b, 0);
+
+//   console.log(sum (salaries) 
+//     );
+
+function objectValueSum (x){
+let valueObject= Object.values(x);
+let sum=0;
+for(let i=0;i<valueObject.length;i++) {
+    
+    sum+=valueObject[i];
+   
+}
+
+
+if (sum==0) {
+    return 0;
+}
+
+else {
+    return sum;
+}
+
+}
+
+console.log(objectValueSum(salaries));
+
+
+//Exercise 7
+//Rewrite this if using the ternary operator '?':
+
+// if (a + b < 4) {
+//     result = 'Below';
+//   } else {
+//     result = 'Over';
+//   }
+
+  let a=5;
+  let b=0;
+
+  let opIF= (a+b<4) ? result='below' : result='over';
+
+  console.log(opIF);
+
+//Exercise 8
+//Rewrite if..else using multiple ternary operators '?'.
+
+
+
+
+
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+
+let message= (login == 'Employee')?  message = 'Hello': (login == 'Director')? message = 'Greetings' : (login == '') ? message = 'No login' :  message = ''
