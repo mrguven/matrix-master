8504038e0dcf9d0355a0e5bd87572e0e22fab1d4
Rauf Guven <mrguven@gmail.com>
2023-05-04 21:27:42 +0200

    update

---
+++ week 3/advanceJs/EcmaScriptExercises.js
@@ -0,0 +1,17 @@
+
+
+// Exercise 1 We have a simple object:
+
+
+
+let user = {
+    name1: "John",
+    years: 30
+  }
+
+
+
+
+  let {name1,years} = user;
+
+  console.log(name1);
--- week 3/algorithm/friday.js
+++ week 3/algorithm/friday.js
@@ -1,1 +1,59 @@
 //3rd Highest Number in Array
+
+
+
+let highestNumber= [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
+
+function findNumber (highestNumber) {
+
+        let firNumber=0;
+        let secNumber=0;
+        let thirdNumber=0;
+
+        for( let i=0;i<highestNumber.length;i++) {
+
+            if(highestNumber[i] >firNumber) {
+                
+                 thirdNumber=secNumber;
+                 secNumber=firNumber;
+                 firNumber=highestNumber[i];
+                
+                
+                    
+                }
+
+                if (highestNumber[i]>secNumber ) {
+                    
+                    thirdNumber=secNumber;
+                    secNumber=highestNumber[i];
+                    
+                    
+                    
+                }
+                if (highestNumber[i]>thirdNumber ) {
+
+                    thirdNumber=highestNumber[i]; 
+
+                   
+                }
+                
+                
+            }
+
+           
+
+             console.log(thirdNumber);
+
+
+}
+
+findNumber(highestNumber);
+
+
+
+
+
+
+
+
+//Friday: Fibonacci Sequence
