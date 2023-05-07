c478ef9484e8bdaf17f32a2ae193e28bf4595b2e
Rauf Guven <mrguven@gmail.com>
2023-05-07 02:53:09 +0200

    update

--- week 3/advanceJs/pomodoro/pomodorClock.js
+++ week 3/advanceJs/pomodoro/pomodorClock.js
@@ -9,135 +9,149 @@ let showBreak= document.getElementById('showBreak');
 let showMin= document.getElementById('showMin')
 
 let sec =document.getElementById('sec')
-
+let secBreak=document.getElementById('secBreak')
 let result = document.getElementById('result');
 
 
 
-result.innerHTML=25;
+result.innerHTML=25+ ":" + "00";
 
-let showBreakContent=showBreak.textContent;
-let showMinContent=showMin.textContent;
-let secContent=sec.textContent;
+let showBreakContent=showBreak.innerText;
+let showMinContent=showMin.innerText;
+let secContent=sec.innerText;
+let secBreakContent=secBreak.innerText;
+console.log(showBreak.innerText);
 
-console.log(showBreak.textContent);
 
 
-
-buttonIncBreak.addEventListener('click', increaseBreakValue)
+buttonIncBreak.addEventListener('click', increaseBreakValue);
 
 function increaseBreakValue (){
-
  showBreakContent=parseInt(showBreakContent);
-
  showBreakContent+=1;
-
  showBreak.innerHTML= showBreakContent;
 
 }
 
 
-buttonDecBreak.addEventListener('click', decBreakValue)
+buttonDecBreak.addEventListener('click', decBreakValue);
 
 function decBreakValue (){
-
  showBreakContent=parseInt(showBreakContent);
-
  showBreakContent-=1;
-
  if(showBreakContent===0 || showBreakContent<0 ) {  // there is beter solution??? but for now I found this.
     showBreakContent=0
  }
 
-
  showBreak.innerHTML= showBreakContent;
 
 }
 
-
-
-
-
-
 buttonIncMin.addEventListener('click', increaseMinValue)
 
 function increaseMinValue (){
-
     showMinContent=parseInt(showMinContent);
-
     showMinContent+=1;
-
     showMin.innerHTML= showMinContent;
     result.innerHTML=showMinContent;
 
 }
 
-
 buttondecMin.addEventListener('click', decMinValue)
 
 function decMinValue (){
-
     showMinContent=parseInt(showMinContent);
 
     showMinContent-=1;
     if(showMinContent===0 || showMinContent<0 ) {  // there is beter solution??? but for now I found this.
         showMinContent=0
      }
-
     showMin.innerHTML= showMinContent;
     result.innerHTML=showMinContent;
 
 }
+let control=true; // to prevent run function for second time 
+let stopInternal; // to stop setInterval method
 
-function runFunction () {setInterval(beginClock, 1000);}
-
+ function runFunction() {
+        if(control) {
+        stopInternal= setInterval(beginClock, 1000);
+        control=false;
+    }
+    
+    else {
+        alert('you already start the clock')
+    }
+    }
 
 result.addEventListener('click',runFunction )
 
-
+secBreakContent=0;
+secContent=0;
 
 function beginClock () {
-    
-
-    
-       
         if(secContent!=0) {
             console.log(secContent);
-            secContent=parseInt(secContent);
-            secContent=60;
             secContent--; }
-
-           else if(secContent==0 && showMinContent!=0) {
+        else if(secContent==0 && showMinContent!=0) {
             secContent=59;
                 showMinContent--;
             }
+           
+        if(secContent==0 && showMinContent==0) {
             
+            if(secBreakContent!=0) {
+                console.log(secBreakContent);
+                secBreakContent--; }
+            
+            else if(showBreakContent!=0 && secBreakContent==0) {
+                secBreakContent=59;
+                showBreakContent--;
 
-
-            if(secContent==0 && showMinContent==0) {
-                if(showBreakContent !=0) {
-                    showBreakContent--;
+                }
+                if(showBreakContent==0 && secBreakContent==0) {
+                    clearInterval(stopInternal);
+                    alert('perfect')
                 }
             }
 
-
-        
-       
-        
+            showBreak.innerHTML= showBreakContent;
+    secBreak.innerHTML=secBreakContent;
    
-    
-    
     result.innerHTML=showMinContent +":" + secContent ;
+}
+
+showBreak.addEventListener('click', runBreak);
+
+let runBreakInterval;
+function runBreak() {
+    runBreakInterval = setInterval(startBreak,1000)
+}
+
+
+
+function startBreak() {
     
-   
+    clearInterval(stopInternal);
+    control=true;
+if(secBreakContent!=0) {
+    console.log(secBreakContent);
+    secBreakContent--; }
 
-
-
-
-   
-
+else if(showBreakContent!=0 && secBreakContent==0) {
+    secBreakContent=59;
+    showBreakContent--;
+    }
     
-    
+if(showBreakContent==0 && secBreakContent==0) {
+    clearInterval(runBreakInterval);
+    alert('perfect')
+}
+
+    showBreak.innerHTML= showBreakContent;
+    secBreak.innerHTML=secBreakContent;
+
+
 }
 
 
