389c6b771837e89ac2e898538ed6fe0272e39445
Rauf Guven <mrguven@gmail.com>
2023-05-10 01:59:11 +0200

    update

--- week 3/OOP/oop.js
+++ week 3/OOP/oop.js
@@ -1,3 +1,20 @@
+
+
+// I am looking for a skilled web developer to create a beauty 
+// website that will allow my clients to pay and book their appointments.
+//  The website will focus on skin care services only. 
+//  I require an integrated calendar feature that will allow clients 
+//  to see my availability and schedule appointments accordingly. Payment 
+//  processing will be done through credit/debit card only. The ideal candidate
+//   will have experience in web development and payment processing.
+
+
+
+
+
+
+
+
 // const myPromise = new Promise(resolve,reject)
 
 
@@ -26,25 +43,44 @@ gril.introduceyourself();
 
 
 
-class Commnent extends Person {
+class Comment extends Person {
 
-post;
-userComment;
-constructor (post,userComment,name1){
+
+constructor (name1,post,userComment){
 super(name1);
 
 this.post= post;
 this.userComment=userComment;
 
 }
-addPost (){ console.log(`this is my ${this.post} and ${this.userComment} ${this.name1}`)
+addPost (){  console.log(`this is my ${this.post} and ${this.userComment} ${this.name1}`)
 
 }
 
 }
 
-let userPost= new Commnent('name ','asdfasdf');
+let userPost= new Comment('name ','asdfasdf','this is my name');
 
-//console.log(userPost)
+console.log(userPost)
 
-console.log(userPost.addPost());
+userPost.addPost();
+
+
+
+const myPromise= new Promise((resolve,reject) => {
+    if(Math.random()*100<90) {
+        console.log('resolving the promise..');
+        resolve('Hello,Promises')
+    }
+    reject(new Error('In 10% of the case, I fail. Miserable'))
+
+});
+
+
+myPromise.then(
+    
+)
+
+
+
+
