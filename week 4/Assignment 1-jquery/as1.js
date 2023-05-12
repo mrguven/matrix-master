ef93365ff4651c76d0d1931dd74db47a72bc15ac
Rauf Guven <mrguven@gmail.com>
2023-05-12 14:31:30 +0200

    update

--- week 4/Assignment 1-jquery/as1.js
+++ week 4/Assignment 1-jquery/as1.js
@@ -1,3 +1,46 @@
 
 
-$('.changeColor')
+
+
+$('#btn-fadeOut').click(function(){
+    $('#text-fadeOut').fadeOut(3000)
+})
+
+
+
+$('#btn-append').click(function() {
+    function NewConstructor (title,auther,year) {
+        this.title=title;
+        this.auther=auther;
+        this.year=year;
+        
+        this.sendOutPut=function() { return `this is ${this.title}, and author is ${this.auther}, publish date is ${this.year}`
+        }
+        }
+        
+            
+        let newUser = new NewConstructor($('#userInput').val(),$('#auther').val(),$('#year').val())
+        let result=newUser.sendOutPut()
+     
+     
+        console.log($('#userInput').val());
+    $('#idList').append('<li>',result, '</li>') // ?? I can not this code. 
+    
+    })
+
+
+
+
+    $('#btn-append').click(function() {
+       
+        console.log( $('#userInput').val())
+    })
+
+
+
+
+
+
+$('#changeColor').click(function(){
+    $('#textColor').css('color','red')
+})
---
+++ week 4/Assignment 2-jquery/a2.js
@@ -0,0 +1,4 @@
+
+$('img').click(function() {
+    console.log('alt-pic is', $(this).attr('1a.jpg'));
+});
