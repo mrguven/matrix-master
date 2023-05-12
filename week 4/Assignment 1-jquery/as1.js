9a8697d3fb67dd0a3c043b6fe8bbec4b7ce9c86f
Rauf Guven <mrguven@gmail.com>
2023-05-12 17:11:38 +0200

    update

--- week 4/Assignment 1-jquery/as1.js
+++ week 4/Assignment 1-jquery/as1.js
@@ -1,7 +1,3 @@
-
-
-
-
 $('#btn-fadeOut').click(function(){
     $('#text-fadeOut').fadeOut(3000)
 })
@@ -10,37 +6,21 @@ function NewConstructor (title,auther,year) {
     this.auther=auther;
     this.year=year;
     
-    this.sendOutPut=function() { return `this is ${this.title}, and author is ${this.auther}, publish date is ${this.year}`
-    }
+    this.sendOutPut=function() { return `this is ${this.title}, 
+    and author is ${this.auther}, publish date is ${this.year}`}
     }
 
 
 $('#btn-append').click(function() {
-   
-        
-            
         let newUser = new NewConstructor($('#userInput').val(),$('#auther').val(),$('#year').val())
         let result=newUser.sendOutPut()
-     
-     
         console.log($('#userInput').val());
     $('#idList').append('<li>',result, '</li>') // ?? I can not this code. 
-    
     })
-
-
-
-
     $('#btn-append').click(function() {
        
         console.log( $('#userInput').val())
     })
-
-
-
-
-
-
 $('#changeColor').click(function(){
     $('#textColor').css('color','red')
 })
