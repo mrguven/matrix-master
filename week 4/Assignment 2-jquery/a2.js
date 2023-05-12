f5ceebfbfef46579239a99cb8fe53d9a3e3419a5
Rauf Guven <mrguven@gmail.com>
2023-05-12 23:19:14 +0200

    update

--- week 4/Assignment 2-jquery/a2.js
+++ week 4/Assignment 2-jquery/a2.js
@@ -1,4 +1,49 @@
 
-$('img').click(function() {
-    console.log('alt-pic is', $(this).attr('1a.jpg'));
-});
+
+let i=0;
+    $('#firstImg').click(function() {
+        let img=["1a.jpg" ,"1b.jpg" ];
+        i+=1
+       if(i>img.length-1){
+        console.log(img.length);
+        i=0;}
+       $('#firstImg').attr('src', img[i] );
+    })
+    let j=0;
+    $('#secImg').click(function() {
+        let img=["2a.jpg" ,"2b.jpg" ];
+        j+=1
+        if(j>img.length-1){
+            console.log(img.length);
+        j=0;}
+       $('#secImg').attr('src', img[j] );
+    })
+    let k=0;
+    $('#thirdImg').click(function() {
+        let img=["3a.jpg" ,"3b.jpg" ];
+        k+=1
+        if(k>img.length-1){
+        k=0;}
+       $('#thirdImg').attr('src', img[k] );
+    })
+
+
+
+
+    
+// $(document).ready(
+//     $('#firstImg').click(function() {
+//         $('#firstImg').attr('src', "1b.jpg" )
+//         }),
+
+
+
+//     $('#secImg').click(function() {
+//         $('#secImg').attr('src', "2b.jpg" )
+//         }),
+
+
+//     $('#thirdImg').click(function() {
+//         $('#thirdImg').attr('src', "3b.jpg" )
+//         })
+// )
---
+++ week 4/Assignment 3-jquery/countdown.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+$('.#btn-start').click(function () {
+    $('.#screen').val('25:00:00')
+    console.log( $('.#screen').val('25:00:00'));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// screen.val().keypress( function() {
+//     onlyNumberKey(evt)})
+// function onlyNumberKey(evt) {
+              
+//     // Only ASCII character in that range allowed
+//     var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+//     if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+//         return false;
+//     return true;
+// }
