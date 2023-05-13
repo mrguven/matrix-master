b95a32d4ae87964ce9c1b256b925d4570ac39b69
Rauf Guven <mrguven@gmail.com>
2023-05-13 23:36:44 +0200

    update

--- week 4/jQueryCrashCourse/jqueryjson.js
+++ week 4/jQueryCrashCourse/jqueryjson.js
@@ -2,11 +2,11 @@
 //$('button .submit') this is to select button element with submit class name
 
 $('#getdt').click(function (){
-    getData ()
-    console.log('info.txt');
+    getData_1()
+    
 })
 
-function getData () {
+function getData_1 () {
 let xhttp= new XMLHttpRequest();
 
 xhttp.onreadystatechange=function(){
@@ -17,12 +17,45 @@ xhttp.onreadystatechange=function(){
        // $('#result').val(screen); it does not work?????
 
     }
-
-
 };
-
 xhttp.open('GET',"info.txt",true);
-
 xhttp.send();
-
 }
+
+
+$('#get-Data').click(function () {
+    httpRequest('GET','https://jsonplaceholder.typicode.com/posts/1').then((respondData)=> {let returnData= respondData;
+    document.getElementById('show-Data').innerHTML=returnData;
+    
+    console.log(returnData);
+});
+})
+
+
+let httpRequest = (method,url,data)=> {
+    let promise = new Promise((res,rej)=>{
+        let newXhttp= new XMLHttpRequest();
+        newXhttp.open(method,url);
+            newXhttp.responseType='json';
+            if(data) {
+            newXhttp.setRequestHeader('Content-Type', 'application/json')
+            }
+        newXhttp.onload=()=> {
+           res(newXhttp.response);
+        }
+        newXhttp.onerror=()=>{
+            rej('something went wrong')
+        }
+        newXhttp.send(JSON.stringify(data));
+    })
+return promise;
+}
+
+
+
+$('#send-data').click(function () {
+    httpRequest('PUT','https://jsonplaceholder.typicode.com/posts/1',
+    { email:'test@askdf.com'}).then((respondData) => 
+    console.log(respondData)).catch((err) => console.log(err))
+})
+
