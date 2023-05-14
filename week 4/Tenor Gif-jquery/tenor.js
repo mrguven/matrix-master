17adfe248cddbd8d6ade6728570c5a3bf82c6d50
Rauf Guven <mrguven@gmail.com>
2023-05-14 11:04:33 +0200

    update

--- week 4/Tenor Gif-jquery/tenor.js
+++ week 4/Tenor Gif-jquery/tenor.js
@@ -2,15 +2,28 @@ $('#btn-submit').click(function(){
 
 })
 
-let httpRequestAction=(method,theUrl,condi)=>{
-
+let httpRequestAction=(method,theUrl,data)=>{
+    let promise = new Promise((res,rej) => {
 let xmlrequest=new XMLHttpRequest();
 
+xmlrequest.open(method,theUrl)
+
+xmlrequest.onload=()=>{
+    res(xmlrequest.response)
+}
+
+xmlrequest.onerror=()=> {
+    rej((err)=> console.log(err))
+}
+
+xmlrequest.send(JSON.stringify(data))
+
+})
 
 
-xmlrequest.onreadystatechange=()=>{
+
+
 
 }
 
 
-}
--- week 4/jQueryCrashCourse/jqueryjson.js
+++ week 4/jQueryCrashCourse/jqueryjson.js
@@ -12,7 +12,7 @@ let xhttp= new XMLHttpRequest();
 xhttp.onreadystatechange=function(){
 
     if(this.readyState==4 && this.status==200)  {
-        let screen = this.responseText
+       // let screen = this.responseText
         document.getElementById('result').innerHTML=this.responseText;
        // $('#result').val(screen); it does not work?????
 
