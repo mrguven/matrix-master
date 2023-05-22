70cd14f6865665d2c444e0a281833d2308041c66
Rauf Guven <mrguven@gmail.com>
2023-05-22 14:18:59 +0200

    update

--- week 5/node.js
+++ week 5/node.js
@@ -2,8 +2,11 @@ let http= require('http');
 
 http.createServer(function (req,res)  {
     
-    res.writeHead(200,{'Content-Type': 'text/html'})
-    res.end('Hello World')
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write('erssdasdfsdfdsfldf   ');
+    res.end('hello world');
+    
+    
 
 }).listen(8080)
 
@@ -37,14 +40,50 @@ fs.writeFile('./test.txt', 'it is write',()=> {
 console.log( 'file is written');
 });
 
-fs.writeFile()
+fs.writeFile('./test1.txt', 'this is test' , ()=> {
+    console.log('the file is written');
+})
 
 
 
 //directories
 
 
+if(!fs.existsSync ('./assets'))  {
+
+fs.mkdir('./assets' , (err)=> {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('folder created');}
+            
+})
+}else {
+    fs.rmdir('./assets', (err)=> {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('the folder deleted');
+        }
+    })
+}
+
+
+
+
+
 
 
 
 //deleting file
+
+
+if(fs.existsSync('./deleteme.txt')) {
+    fs.unlink('./deleteme.txt', (err)=> {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('the folder is deleted');
+        }
+    })
+}
