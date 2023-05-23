3db606cccf7964d54bf7a05e2801310081fef15a
Rauf Guven <mrguven@gmail.com>
2023-05-23 17:34:57 +0200

    update

--- week 5/clients.js
+++ week 5/clients.js
@@ -1,13 +1,30 @@
 let http = require('http');
+let fs=require('fs')
 
 let server= http.createServer((req,res)=> {
     console.log(req.url,req.method);
 
     
     res.setHeader('Content-Type','text/html');
-    res.write( '<img src="download.jpeg"></img>' )
-    res.write('<h1>is is ninja</h1>');
-    res.end()
+    // res.write( '<img src="download.jpeg"></img>' ) 
+    // res.write('<h1>is is ninja</h1>'); this is used for text to send to browser
+
+    // to send  the html file to browser of and data, we use first fs. first we read the file and then 
+    // we write the data 
+    fs.readFileSync('./index.html', (err,data)=> {
+        if(err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            //res.write(data); // if we send only one data we use end method
+            res.end(data);
+        }
+    })
+
+
+
+   
 });
 //local host is like a domain name on the web
 //port number is like doors into a computer
