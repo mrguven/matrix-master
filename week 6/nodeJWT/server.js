28ee6089c256aa704a1ede289685ce0fd7540a6e
Rauf Guven <mrguven@gmail.com>
2023-06-05 20:14:05 +0200

    update

--- week 6/nodeJWT/server.js
+++ week 6/nodeJWT/server.js
@@ -1,16 +1,19 @@
 const express =require('express');
 const app= express();
+const env= require('dotenv').config();
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+app.set('view engine','ejs');
+
+const port= process.env.PORT
 
 
 
 
-
-app.listen(3344, ()=> {
+app.listen(port, ()=> {
     console.log('created the port');
 })
