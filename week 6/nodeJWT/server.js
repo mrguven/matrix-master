621c446cb6dd35f81bf421fccc07e1a9b12ef5bb
Rauf Guven <mrguven@gmail.com>
2023-06-05 19:21:33 +0200

    update

--- week 6/nodeJWT/server.js
+++ week 6/nodeJWT/server.js
@@ -2,6 +2,15 @@ const express =require('express');
 const app= express();
 
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+
+
+
+
 app.listen(3344, ()=> {
     console.log('created the port');
 })
